2b66b994ce02d601bb0eefd6e5de1a6b2180c92b
lzp <lzp@LZP>
2019-12-16 17:05:04 +0800

    1216

--- test1127-1/src/concurrency/AtomicityTest.java
+++ test1127-1/src/concurrency/AtomicityTest.java
@@ -1,42 +1,50 @@
 package concurrency;
-import java.util.concurrent.*;;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.*;
 
 public class AtomicityTest implements Runnable {
+	private static Lock lock = new ReentrantLock();
 
-	private static volatile  int i=0;
+	private static int i = 0;
+
 	public synchronized int getValue() {
-		return i;
+		synchronized(this.getClass()){
+		return i;}
 	}
-	private synchronized void evenIncrement() {
-		//i+=2;
-		 {
-			i++;
-//			try {
-//				TimeUnit.MILLISECONDS.sleep(20);
-//			} catch (InterruptedException e) {
-//				// TODO Auto-generated catch block
-//				e.printStackTrace();
-//			}
-		//	Thread.yield();
-			i++;
-		}
 
+	private synchronized void evenIncrement() {
+	//	if (lock.tryLock()) {
+		synchronized(this.getClass()){
+				i++;
+				// try {
+				// TimeUnit.MILLISECONDS.sleep(20);
+				// } catch (InterruptedException e) {
+				// // TODO Auto-generated catch block
+				// e.printStackTrace();
+				// }
+				Thread.yield();
+				i++;
+			}
+	//	}
 	}
-	public  void run() {
-		while(true)
+
+	public void run() {
+		while (true)
 			evenIncrement();
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ExecutorService exec=Executors.newCachedThreadPool();
-		AtomicityTest at=new AtomicityTest();
-		for(int i=0;i<10;i++)
+		ExecutorService exec = Executors.newCachedThreadPool();
+		AtomicityTest at = new AtomicityTest();
+		exec.execute(at);
+		for (int i = 0; i < 10; i++)
 			exec.execute(new AtomicityTest());
-		while(true) {
-			int val=at.getValue();
-		//	System.out.println(val);
-			if(val%2!=0) {
+		while (true) {
+			int val = at.getValue();
+			 System.out.println(val);
+			if (val % 2 != 0) {
 				System.out.println(val);
 				System.exit(0);
 			}
--- test1127-1/src/concurrency/CriticalSection.java
+++ test1127-1/src/concurrency/CriticalSection.java
@@ -2,10 +2,13 @@ package concurrency;
 
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.*;
+
+import javax.sound.midi.Track;
+
 import java.util.*;
 
 class Pair {
-	private int x, y;
+	private volatile int x, y;
 
 	public Pair(int x, int y) {
 		this.x = x;
@@ -42,16 +45,19 @@ class Pair {
 		}
 	}
 
-	public void checkState() {
-		if (x != y)
+	public synchronized void checkState() {
+		if (x != y){
+			System.out.println(this.getClass().getSimpleName());
 			throw new PairValuesNotEqualException();
+		}
 	}
 }
 
 abstract class PairManager {
 	AtomicInteger checkCounter = new AtomicInteger(0);
 	protected Pair p = new Pair();
-	private List<Pair> storage = Collections.synchronizedList(new ArrayList<Pair>());
+	//private List<Pair> storage = Collections.synchronizedList(new ArrayList<Pair>());
+	private List<Pair> storage = new ArrayList<Pair>();
 
 	public synchronized Pair getPair() {
 		return new Pair(p.getX(), p.getY());
@@ -59,10 +65,10 @@ abstract class PairManager {
 
 	protected void store(Pair p) {
 		storage.add(p);
-		try {
-			TimeUnit.MILLISECONDS.sleep(50);
-		} catch (InterruptedException ignore) {
-		}
+//		try {
+//			TimeUnit.MILLISECONDS.sleep(50);
+//		} catch (InterruptedException ignore) {
+//		}
 	}
 
 	public abstract void increment();
